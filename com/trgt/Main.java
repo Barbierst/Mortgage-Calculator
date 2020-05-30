377b8ebc18ff02f125f464548e651cda073030a3
timbarbiers <tim.barbiers@gmail.com>
2020-05-30 22:27:31 +0200

    Code with mosh java youtube serie repo

---
+++ com/trgt/Main.java
@@ -0,0 +1,14 @@
+package com.trgt;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = (double)
+
+        int[][] numbers = {{12, 2, 3, 4, 5}, {4 ,3,4,2}};
+
+	    System.out.println(Arrays.deepToString(numbers));
+    }
+}
